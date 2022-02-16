cfa863d914736fa99a4a24712f822b35a254e2a5
ADAUTO LUCAS <adautosl.lucas@gmail.com>
2022-02-16 14:45:17 -0300

    instalado dependencias do typeorm, nestjs-query, criado o arquivo docker com postgres e criado o arquivo .env para conexao com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
